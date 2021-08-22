6b3b6be376d1cdda6e99da848ce13bf3958e502b
paynd <a.kupreychik@gmail.com>
2021-08-22 17:42:48 +0300

    Time formatting and minor refactoring

--- feature_articles/src/main/java/name/paynd/android/newsapi/articles/ArticlesAdapter.kt
+++ feature_articles/src/main/java/name/paynd/android/newsapi/articles/ArticlesAdapter.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -27,14 +28,6 @@ class ArticlesAdapter(context: Context) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticlesViewHolder {
         return ArticlesViewHolder(layoutInflater.inflate(R.layout.article_item, parent, false))
     }
-
-    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
-        super.onAttachedToRecyclerView(recyclerView)
-    }
-
-    override fun getItemCount(): Int {
-        return super.getItemCount()
-    }
 }
 
 class ArticlesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -42,24 +35,33 @@ class ArticlesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(article: Article?) {
         with(viewBinding) {
-            image.load(article?.urlToImage) {
-                placeholder(ColorDrawable(Color.TRANSPARENT))
+            article?.let { item ->
+                image.load(item.urlToImage) {
+                    placeholder(ColorDrawable(Color.TRANSPARENT))
+                }
+                with(item) {
+                    itemAuthor.setString(author)
+                    itemDescription.setString(description)
+                    itemTitle.setString(title)
+                    itemDate.setString(publishedAt)
+                }
             }
-            itemAuthor.text = article?.author
-            itemDescription.text = article?.description
-            itemTitle.text = article?.title
         }
+    }
+}
+
+fun TextView.setString(text: String?) {
+    text?.let {
+        this.text = it
     }
 }
 
 private object ArticleDiffItemCallback : DiffUtil.ItemCallback<Article>() {
     override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
         return oldItem == newItem
-//        return false
     }
 
     override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
         return oldItem.title == newItem.title && oldItem.url == newItem.url
-//        return false
     }
 }
--- feature_articles/src/main/java/name/paynd/android/newsapi/articles/model/Article.kt
+++ feature_articles/src/main/java/name/paynd/android/newsapi/articles/model/Article.kt
@@ -1,6 +1,9 @@
 package name.paynd.android.newsapi.articles.model
 
 import name.paynd.android.newsapi.api.ArticleDto
+import org.ocpsoft.prettytime.PrettyTime
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class Article(
     val title: String,
@@ -19,7 +22,12 @@ internal fun ArticleDto.toArticle(): Article {
         description = description,
         author = author,
         urlToImage = urlToImage,
-        publishedAt = publishedAt,
+        publishedAt = publishedAt?.parseDate()?.getOrNull(), //ISO 8601 yyyy-MM-dd'T'HH:mm:ss
         content = content
     )
+}
+
+fun String.parseDate() = runCatching {
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+    PrettyTime().format(sdf.parse(this))
 }
